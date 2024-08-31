fd281cc76e9de7433bd51cd2517a1a6493eb85b5
Paulo Ricardo dos Reis <paulo230301@hotmail.com>
2024-08-31 15:59:53 -0300

    estudos

--- React/3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ React/3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -5,10 +5,24 @@ import ManageData from "./components/ManageData";
 import ListRender from "./components/ListRender";
 import ConditionRender from "./components/ConditionRender";
 import ShowUserName from "./components/ShowUserName";
+import CarDetails from "./components/CarDetails";
+import Fragments from "./components/Fragments";
+import Container from "./components/Container";
+import ExecuteFunction from "./components/ExecuteFunction";
 
 function App() {
-  const name = "João";
+  //const name = "João";
   const [userName] = useState("Maria");
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Vermelha", newCar: true, km: 0 },
+    { id: 2, brand: "Fiat", color: "Branco", newCar: false, km: 15000 },
+    { id: 3, brand: "Nissan", color: "Preto", newCar: false, km: 50000 },
+  ];
+
+  function showMessage() {
+    console.log("Evento do componente pai!");
+  }
 
   return (
     <div>
@@ -24,7 +38,31 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionRender />
+      {/* props */}
       <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="Nissan" km={100000} color="Azul" newCar={false} />
+      {/* reaproveitando */}
+      <CarDetails brand="Ford" km={0} color="Vermelha" newCar={true} />
+      <CarDetails brand="Fiat" km={660000} color="Preto" newCar={false} />
+      {/* loop em array de obj */}
+      {cars.map((car) => (
+        <CarDetails
+          key={car.id}
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/* Fragments */}
+      <Fragments propFragment="teste" />
+      {/* Children */}
+      <Container myValue="teste">
+        <p>E este é o conteúdo</p>
+      </Container>
+      {/* executar função */}
+      <ExecuteFunction myFunction={showMessage} />
     </div>
   );
 }
